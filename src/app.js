2ed2fd8ddd15a2fb9a8b16efd475a0a616248fa6
Felipe Oliveira <felipe.oliveirac8@gmail.com>
2020-02-10 16:48:40 -0300

    Config database connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
 const express = require('express');
+const mongose = require('mongoose');
+require('dotenv').config();
+
+// Database
+mongose.connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongose.connection;
+
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+db.on('error', err => {
+    console.log(`Mongoose default connection has occured \n${err}`);
+});
+
+db.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log(
+            'Mongoose default connection is disconnected due to application termination'
+        );
+        process.exit(0);
+    });
+});
 
 // App
 const app = express();
