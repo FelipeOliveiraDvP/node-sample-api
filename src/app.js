c20c4e68c4d24bfbe6053d2e4daf5520e0373344
Felipe Oliveira <felipe.oliveirac8@gmail.com>
2020-02-11 09:34:11 -0300

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Enable CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+    next();
+});
+
 // Database
 mongose.connect(process.env.DATABASE_CONNECTION_STRING, {
     useNewUrlParser: true,
